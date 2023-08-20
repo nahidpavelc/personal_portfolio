cdf5770a61e150da8c12d9dc0ef91264b1097e50
nahidpavelc <nahidpavelchowdhury777@gmail.com>
2023-08-20 22:57:36 +0600

    Compleate Navbar

--- components/Nav.js
+++ components/Nav.js
@@ -10,7 +10,7 @@ import {
 
 //  nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome/> },
+  { name: 'home', path: '/', icon: <HiHome /> },
   { name: 'about', path: '/about', icon: <HiUser /> },
   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
   { name: 'work', path: '/work', icon: <HiViewColumns /> },
@@ -40,10 +40,30 @@ const Nav = () => {
   return (
     <nav className='flex flex-col items-center xl:justify-center gap-y4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
       {/* inner  */}
-      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
 
         {navData.map((link, index) => {
-          return <Link href={link.path}> {link.icon} </Link>;
+          return (
+            <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300`}
+              href={link.path}
+              key={index}>
+              {/* tooltip */}
+              <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                <div className='bg-white relative flex text-primary items-center p-[6px]  rounded-[3px]'>
+                  <div className='text-[12px] leading-none font-semibold capitalize'>
+                    {link.name}
+                  </div>
+                  {/* triangle */}
+                  <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+                </div>
+              </div>
+              
+              {/* icon */}
+              <div>
+                {link.icon}
+              </div>
+            </Link>
+          );
         })}
 
       </div>
