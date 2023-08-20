a196d0f0dd57bef05d1275a5b1995237a8d63fb0
nahidpavelc <nahidpavelchowdhury777@gmail.com>
2023-08-20 22:27:35 +0600

    update navbar

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import { Sora } from '@next/font/google'
 const sora = Sora({
   subsets: ['latin'],
   variable: '--font-sora',
-  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
 });
 
 
@@ -16,12 +16,15 @@ import TopLeftImg from '../components/TopLeftImg';
 
 
 const Layout = ({children}) => {
-  return <div>
-    <TopLeftImg/>
-    <Nav />
-    <Header />
-    {children}
-  </div>;
+  return (
+    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative `}>
+
+      <TopLeftImg />
+      <Nav />
+      <Header /> 
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,16 @@
-//  links
-const links = [
-  { name: 'home', path: '/', icon: <HiHome /> },
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope
+} from 'react-icons/hi2';
+
+//  nav data
+export const navData = [
+  { name: 'home', path: '/', icon: <HiHome/> },
   { name: 'about', path: '/about', icon: <HiUser /> },
   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
   { name: 'work', path: '/work', icon: <HiViewColumns /> },
@@ -16,8 +26,29 @@ const links = [
   },
 ];
 
+//next Link
+import Link from 'next/link';
+
+//next router
+import { useRouter } from 'next/router';
+
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  return (
+    <nav className='flex flex-col items-center xl:justify-center gap-y4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+      {/* inner  */}
+      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+
+        {navData.map((link, index) => {
+          return <Link href={link.path}> {link.icon} </Link>;
+        })}
+
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
