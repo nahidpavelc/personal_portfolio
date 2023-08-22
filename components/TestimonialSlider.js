c30cd7ccaf708ced90d9c4f86661695bd6e7b6a5
nahidpavelc <nahidpavelchowdhury777@gmail.com>
2023-08-23 02:56:47 +0600

    compleate Testimonial

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -28,11 +28,11 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // import swiper styles
 import 'swiper/css';
-import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // import required modules 
-import { Pagination } from 'swiper';
+import { Navigation, Pagination } from 'swiper';
 
 // icons
 import { FaQuoteLeft } from 'react-icons/fa';
@@ -42,37 +42,42 @@ import Image from 'next/image';
 function TestimonialSlider() {
   return (
     <Swiper
+      navigation={true}
       spaceBetween={10}
       pagination={{
-        clickable: true
+        clickable: true,
       }}
-      modules={[Pagination]}
-      className='h-[280px] sm:h-[480px]'
+      modules={[Navigation, Pagination]}
+      className='h-[400px]'
     >
       {testimonialData.map((person, index) => {
         return (
           <SwiperSlide key={index}>
-            <div>
+            <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
               {/* avatar, name, position */}
-              <div>
-                <div>
+              <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                <div className='flex flex-col justify-center text-center'>
                   {/* avatar  */}
-                  <div>Avatar Image</div>
+                  <div className='mb-2 mx-auto'>
+                    <Image src={person.image} width={100} height={100} alt='' />
+                  </div>
                   {/* name  */}
-                  <div>name</div>
+                  <div className='text-lg'>{person.name}</div>
                   {/* position  */}
-                  <div>position</div>
+                  <div className='text-[12px] uppercase font-extralight tracking-widest'>{person.position}</div>
                 </div>
               </div>
 
               {/* quote & message */}
-              <div className='bg-pink-500/10 flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+              <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
                 {/* quote icon  */}
                 <div className='mb-4'>
                   <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
                 </div>
                 {/* message */}
-                <div>{person.message}</div>
+                <div className='xl:text-lg text-center md:text-left'>
+                  {person.message}
+                </div>
               </div>
             </div>
           </SwiperSlide>
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,31 @@
+// components 
+import Circles from '/components/Circles';
+
+// icons 
+import { BsArrowRight } from 'react-icons/bs';
+
+// framer
+import { motion } from 'framer-motion';
+
+// variants
+import { fadeIn } from '../../variants'
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='h-full bg-primary/30'>
+      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
+        {/* text & form */}
+        <div className=''>
+          {/* text */}
+          <h2 className='h2 text-center mb-12'>
+            Let's <span className='text-accent'>Connect.</span>
+          </h2>
+          {/* form */}
+          <form action="">form</form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
