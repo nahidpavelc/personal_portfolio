3d3c7cdc1a2edfe42bfdb4c345f3235a5ad3d519
nahidpavelc <nahidpavelchowdhury777@gmail.com>
2023-08-23 02:02:50 +0600

    compleate Work

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -23,8 +23,64 @@ const testimonialData = [
   },
 ];
 
-const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
-};
+// import swiper  react  components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// import required modules 
+import { Pagination } from 'swiper';
+
+// icons
+import { FaQuoteLeft } from 'react-icons/fa';
+// next image 
+import Image from 'next/image';
+
+function TestimonialSlider() {
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true
+      }}
+      modules={[Pagination]}
+      className='h-[280px] sm:h-[480px]'
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div>
+              {/* avatar, name, position */}
+              <div>
+                <div>
+                  {/* avatar  */}
+                  <div>Avatar Image</div>
+                  {/* name  */}
+                  <div>name</div>
+                  {/* position  */}
+                  <div>position</div>
+                </div>
+              </div>
+
+              {/* quote & message */}
+              <div className='bg-pink-500/10 flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+                {/* quote icon  */}
+                <div className='mb-4'>
+                  <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
+                </div>
+                {/* message */}
+                <div>{person.message}</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+      Service Slider</Swiper>
+  );
+}
 
 export default TestimonialSlider;
+
